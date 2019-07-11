898dd2091daf8429d1aaa6de4d8f8d6244e40400
github username <github email address>
2019-07-10 21:47:31 -0400

    add space and seperator between Nav bar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/">Home</NavLink> <span> | </span>
+      <NavLink to="/movies">Movies</NavLink> <span> | </span>
+      <NavLink to="/directors">Directors</NavLink> <span> | </span>
+      <NavLink to="/actors">Actors</NavLink> <span> | </span>
     </div>
   );
 };
